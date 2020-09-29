94fc49588a821e432c9bb42893198e6dfaabb142
Santiago Cobelli <36997907+cobellisantiago@users.noreply.github.com>
2020-09-29 20:11:42 -0300

    Release 3 (#6)
    * Add feature Big Hero 6
    
    * Add Callahan motive
    
    * Revert "Add Callahan motive"
    
    This reverts commit ce6a04c86999e67c86f0cde618a2f5ea9b1003a3.

--- trabajo-integrador/src/main/java/com/cobelliluetichperezvazquez/trabajointegrador/repository/DomicilioRepository.java
+++ trabajo-integrador/src/main/java/com/cobelliluetichperezvazquez/trabajointegrador/repository/DomicilioRepository.java
@@ -5,5 +5,11 @@ import org.springframework.stereotype.Repository;
 @Repository("DomicilioRepository")
 public class DomicilioRepository {
 
-
+ public String falalala(boolean fistBumpA, boolean fistBumpB) {
+     if(fistBumpA && fistBumpB){
+         return "falalala";
+     }else{
+         return "give me a fist bump Baymax!";
+     }
+ }
 }
